86cc426ef4093b16a6abb5475e6d4c08a42c0d8f
Jesús Torrijo Gimeno <jesustorrijogimeno@gmail.com>
2024-05-08 22:01:48 +0200

    Improved UI visuals

--- Source/WeatherVR/Private/UI/WCitiesSelectorBase.cpp
+++ Source/WeatherVR/Private/UI/WCitiesSelectorBase.cpp
@@ -1,19 +1,48 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "UI/WCitiesSelectorBase.h"
+
+#include "Services/WWeatherService.h"
+#include "Services/Models/FCityStats.h"
 #include "UI/WButtonCityBase.h"
 
-void UWCitiesSelectorBase::SubscribeButtons(UVerticalBox* VerticalBox)
+void UWCitiesSelectorBase::SubscribeButtons(
+	UVerticalBox* VerticalBox,
+	UTextBlock* CityTextBlock,
+	UTextBlock* TemperatureTextBlock,
+	UTextBlock* ConditionTextBlock,
+	UTextBlock* WindDirectionTextBlock,
+	UTextBlock* WindSpeedTextBlock,
+	UTextBlock* CurrentTimeTextBlock
+)
 {
+	this->CityTextBlock				= CityTextBlock;
+	this->TemperatureTextBlock		= TemperatureTextBlock;
+	this->ConditionTextBlock		= ConditionTextBlock;
+	this->WindDirectionTextBlock	= WindDirectionTextBlock;
+	this->WindSpeedTextBlock		= WindSpeedTextBlock;
+	this->CurrentTimeTextBlock		= CurrentTimeTextBlock;
 	for (int32 i = 0; i < VerticalBox->GetChildrenCount(); ++i)
 	{
 		UWidget* ChildWidget = VerticalBox->GetChildAt(i);
 		if (ChildWidget->IsA<UWButtonCityBase>())
 		{
 			UWButtonCityBase* Button = Cast<UWButtonCityBase>(ChildWidget);
-			Button->OnButtonClicked.AddLambda([](const FString& CityName)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("Name Clicked -> %s"), *CityName);
-			});
+			Button->OnButtonClicked.AddLambda(
+				[this](const FString& CityName)
+					{
+						this->CityName = CityName;
+						UWWeatherService::FetchCityWeatherStats(CityName, this->FillInCityStatsWidget);
+					});
 		}
 	}
+}
+
+void UWCitiesSelectorBase::FillInCityStatsWidget(const FCityStats& CityStats) const
+{
+	this->CityTextBlock->SetText(FText::FromString(this->CityName));
+	this->TemperatureTextBlock->SetText(FText::FromString(FString::Printf(TEXT("%f F"), CityStats.Temperature)));
+	this->ConditionTextBlock->SetText(FText::FromString(CityStats.ConditionDescription));
+	this->WindDirectionTextBlock->SetText(FText::AsNumber(CityStats.WindDirection));
+	this->WindSpeedTextBlock->SetText(FText::FromString(FString::Printf(TEXT("%f m/s"), CityStats.WindSpeed)));
+	this->CurrentTimeTextBlock->SetText(FText::AsDateTime(FDateTime::FromUnixTimestamp(CityStats.CurrentTime)));
 }
--- Source/WeatherVR/Public/UI/WCitiesSelectorBase.h
+++ Source/WeatherVR/Public/UI/WCitiesSelectorBase.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "Components/VerticalBox.h"
+#include "Services/Models/FCityStats.h"
 #include "WCitiesSelectorBase.generated.h"
 
 class UWButtonCityBase;
@@ -18,5 +20,28 @@ class WEATHERVR_API UWCitiesSelectorBase : public UUserWidget
 	GENERATED_BODY()
 public:
 	UFUNCTION(BlueprintCallable)
-	void SubscribeButtons(UVerticalBox* VerticalBox);
+	void SubscribeButtons(
+		UVerticalBox* VerticalBox,
+		UTextBlock* CityTextBlock,
+		UTextBlock* TemperatureTextBlock,
+		UTextBlock* ConditionTextBlock,
+		UTextBlock* WindDirectionTextBlock,
+		UTextBlock* WindSpeedTextBlock,
+		UTextBlock* CurrentTimeTextBlock
+	);
+protected:
+	FString CityName;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> CityTextBlock;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> TemperatureTextBlock;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> ConditionTextBlock;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> WindDirectionTextBlock;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> WindSpeedTextBlock;
+	UPROPERTY()
+	TObjectPtr<UTextBlock> CurrentTimeTextBlock;
+	void FillInCityStatsWidget(const FCityStats& CityStats) const;
 };
